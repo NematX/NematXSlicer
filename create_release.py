ef88b557c659fac31e67bbd785246c9ba169f151
supermerill <merill@free.fr>
2026-04-01 13:19:01 +0200

    update win build for updated release script

--- create_release.py
+++ create_release.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from shutil import rmtree
 from urllib.request import Request, urlopen, urlretrieve
 import json
@@ -13,6 +14,7 @@ import time
 from datetime import date
 import tarfile
 import subprocess
+from pathlib import Path
 import argparse
 import re
 
@@ -48,15 +50,38 @@ token = None
 path_7zip = r"C:\Program Files\7-Zip\7z.exe"
 # github classic personal access token, works with [gist, repo, workflow] permissions, should be something like "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"
 github_auth_token = ""
+# try to fill github_auth_token via file
+githubtoken_file = Path("githubtoken.ini")
+if githubtoken_file.exists():
+	with open(githubtoken_file) as f:
+		for line in f:
+			if line.startswith("token="):
+				github_auth_token = line.strip().split("=", 1)[1];
+				print(f"file githubtoken.ini found, using token:'{github_auth_token}'");
+				break
+else:
+	print(f"file githubtoken.ini not found, using hardcoded token:'{github_auth_token}'");
+	if(not github_auth_token):
+		print("For this scipt to work, please create a 'githubtoken.ini' file, and make at least a line with 'token=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'");
 
 def get_version():
 	settings_stream = open("./version.inc", mode="r", encoding="utf-8");
 	lines = settings_stream.read().splitlines();
+	version = "0.0.0.0";
+	tag="";
 	for line in lines:
-		if("SLIC3R_VERSION_FULL" in line):
+		if("set(SLIC3R_RC_VERSION_DOTS" in line):
+			print("found1: " +line)
 			elems = line.split("\"");
-			return elems[1];
-	return "";
+			version = elems[1];
+		if("set(SLIC3R_VERSION_TAG" in line):
+			print("found2: " +line)
+			elems = line.split("\"");
+			tag = elems[1];
+	if("+" in tag):
+		elems = tag.split("+");
+		tag = elems[0];
+	return version+tag;
 
 found_win = False; 
 found_win_msi = False; 
@@ -77,9 +102,9 @@ def handle_artifact(json_artifact):
 	global found_macos_arm
 	global first_day
 	
-	if json_artifact["workflow_run"]["head_branch"] == "rc":
+	if json_artifact["workflow_run"]["head_branch"] == branch_name:
 		if first_day == "":
-			print("encounter the first rc at " + json_artifact["created_at"][:10]);
+			print("encounter the first " + branch_name + " at " + json_artifact["created_at"][:10]);
 			first_day = json_artifact["created_at"][:10];
 		if json_artifact["created_at"][:10] == first_day:
 			print("Next artifact: " + json_artifact["name"]);
@@ -90,14 +115,14 @@ def handle_artifact(json_artifact):
 			print(f"End of {branch_name} artifacts (date too far away). Closing");
 			print("("+json_artifact["name"] + "  @ "+json_artifact["created_at"][:10]+")");
 			return False;
-		if json_artifact["name"] == "rc_win64" and not found_win:
+		if json_artifact["name"] == prefix + "_" +program_name + "-win64" and not found_win:
 			found_win = True;
 			print("Found win64 artifact");
 			print("ask for: "+json_artifact["archive_download_url"]);
 			resp = requests.get(json_artifact["archive_download_url"], headers={'Authorization': 'token ' + github_auth_token,}, allow_redirects=True);
 			print("win: " +str(resp));
 			z = zipfile.ZipFile(io.BytesIO(resp.content))
-			base_name = release_path+"/"+prefix+"_" +program_name+"_"+version+"_win64_"+date_str;
+			base_name = release_path+"/"+program_name+"_"+version+"_win64_"+date_str;
 			z.extractall(base_name);
 			try:
 				ret = subprocess.check_output([path_7zip, "a", "-tzip", base_name+".zip", base_name]);
@@ -121,7 +146,7 @@ def handle_artifact(json_artifact):
 			z = zipfile.ZipFile(io.BytesIO(resp.content));
 			z.extractall(release_path);
 			os.rename(release_path+"/"+program_name+"-macOS-intel.dmg", release_path+"/"+program_name+"_"+version+"_macos_"+date_str+".dmg");
-		if json_artifact["name"] == "rc_"+program_name+"-macOS-arm.dmg" and not found_macos_arm:
+		if json_artifact["name"] == prefix + "_"+program_name+"-macOS-arm.dmg" and not found_macos_arm:
 			found_macos_arm = True;
 			print("Found macos-arm artifact");
 			print("ask for: "+json_artifact["archive_download_url"]);
@@ -130,7 +155,7 @@ def handle_artifact(json_artifact):
 			z = zipfile.ZipFile(io.BytesIO(resp.content));
 			z.extractall(release_path);
 			os.rename(release_path+"/"+program_name+"-macOS-arm.dmg", release_path+"/"+program_name+"_"+version+"_macos_arm_"+date_str+".dmg");
-		if json_artifact["name"] == "rc_"+program_name+"-linux-x64-GTK2.AppImage" and not found_linux_appimage_gtk2:
+		if json_artifact["name"] == prefix + "_"+program_name+"-linux-x64-GTK2.AppImage" and not found_linux_appimage_gtk2:
 			found_linux_appimage_gtk2 = True;
 			print("Found ubuntu GTK2 artifact");
 			print("ask for: "+json_artifact["archive_download_url"]);
@@ -139,7 +164,7 @@ def handle_artifact(json_artifact):
 			z = zipfile.ZipFile(io.BytesIO(resp.content));
 			z.extractall(release_path);
 			os.rename(release_path+"/"+program_name+"-linux-x64-GTK2.AppImage", release_path+"/"+program_name+"-ubuntu_22.04-gtk2-" + version + ".AppImage");
-		if json_artifact["name"] == "rc_"+program_name+"-linux-x64-GTK3.AppImage" and not found_linux_appimage_gtk3:
+		if json_artifact["name"] == prefix + "_"+program_name+"-linux-x64-GTK3.AppImage" and not found_linux_appimage_gtk3:
 			found_linux_appimage_gtk3 = True;
 			print("Found ubuntu GTK3 artifact");
 			print("ask for: "+json_artifact["archive_download_url"]);
@@ -148,7 +173,7 @@ def handle_artifact(json_artifact):
 			z = zipfile.ZipFile(io.BytesIO(resp.content));
 			z.extractall(release_path);
 			os.rename(release_path+"/"+program_name+"-linux-x64-GTK3.AppImage", release_path+"/"+program_name+"-ubuntu_22.04-" + version + ".AppImage");
-		if json_artifact["name"] == "rc_"+program_name+"-linux-x64-GTK3.tgz" and not found_linux:
+		if json_artifact["name"] == prefix + "_"+program_name+"-linux-x64-GTK3.tgz" and not found_linux:
 			found_linux = True;
 			print("Found ubuntu GTK3 archive artifact");
 			print("ask for: "+json_artifact["archive_download_url"]);
