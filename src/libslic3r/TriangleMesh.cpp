3caa1d3cd4ba76a4ccbb94755c8106e6b3ce65ec
supermerill <merill@free.fr>
2026-05-15 07:26:56 +0200

    increased meshslicer precision a bit by using double to scale the coordinates.

--- src/libslic3r/TriangleMesh.cpp
+++ src/libslic3r/TriangleMesh.cpp
@@ -886,11 +886,11 @@ void its_collect_mesh_projection_points_above(const indexed_triangle_set &its, c
             const Vec3f &p2 = pts[iedge];
             if ((p1.z() < z && p2.z() > z) || (p2.z() < z && p1.z() > z)) {
                 // Edge crosses the z plane. Calculate intersection point with the plane.
-                float t = (z - p1.z()) / (p2.z() - p1.z());
-                all_pts.emplace_back(scaled<coord_t>(p1.x() + (p2.x() - p1.x()) * t), scaled<coord_t>(p1.y() + (p2.y() - p1.y()) * t));
+                double t = (z - p1.z()) / (p2.z() - p1.z());
+                all_pts.emplace_back(scale_t(p1.x() + (p2.x() - p1.x()) * t), scale_t(p1.y() + (p2.y() - p1.y()) * t));
             }
             if (p2.z() >= z)
-                all_pts.emplace_back(scaled<coord_t>(p2.x()), scaled<coord_t>(p2.y()));
+                all_pts.emplace_back(scale_t(p2.x()), scale_t(p2.y()));
             iprev = iedge;
         }
     }
@@ -921,11 +921,11 @@ Polygon its_convex_hull_2d_above(const indexed_triangle_set& its, const Transfor
                 const Vec3f& p2 = tri_pts[iedge];
                 if ((p1.z() < z && p2.z() > z) || (p2.z() < z && p1.z() > z)) {
                     // Edge crosses the z plane. Calculate intersection point with the plane.
-                    const float t = (z - p1.z()) / (p2.z() - p1.z());
-                    pts.emplace_back(scaled<coord_t>(p1.x() + (p2.x() - p1.x()) * t), scaled<coord_t>(p1.y() + (p2.y() - p1.y()) * t));
+                    const double t = (z - p1.z()) / (p2.z() - p1.z());
+                    pts.emplace_back(scale_t(p1.x() + (p2.x() - p1.x()) * t), scale_t(p1.y() + (p2.y() - p1.y()) * t));
                 }
                 if (p2.z() >= z)
-                    pts.emplace_back(scaled<coord_t>(p2.x()), scaled<coord_t>(p2.y()));
+                    pts.emplace_back(scale_t(p2.x()), scale_t(p2.y()));
                 iprev = iedge;
             }
         }
